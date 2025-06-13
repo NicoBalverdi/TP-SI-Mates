a3afd3cd88521696b55987404508d780c76afb66
Juan Barbero <juanbarbero40@gmail.com>
2025-06-13 19:13:12 -0300

    se añadio la funcionalidad de bisiesto

--- Semana_Integracion_II.py
+++ Semana_Integracion_II.py
@@ -1,5 +1,9 @@
 # Funciones
 
+def es_bisiesto(año):
+   
+    return (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0)
+
 def digito_comun(lista_conjuntos, digito):
     return all(digito in conjunto for conjunto in lista_conjuntos)
 
@@ -11,100 +15,111 @@ def suma_digitos(numero):
     return suma
 
 # Programa principal
+while True:
+    listaDNI = []
+    listaConjuntos = []
 
-listaDNI = []
-listaConjuntos = []
+    print("== Semana de Integración II - Matemática ==")
 
-print("== Semana de Integración II - Matemática ==")
+    print("Operaciones")
+    print("A. Operaciones con multiples DNI")
+    print("B. Operaciones con años de Nacimiento")
+    opcion = input("Seleccione una opción: ")
+    
+    if not opcion: # Si el usuario solo presiona Enter, sale del bucle
+        break
 
-print("Operaciones")
-print("A. Operaciones con multiples DNI")
-print("B. Operaciones con años de Nacimiento")
-opcion = input("Seleccione una opción: ")
-
-if opcion.lower() == "a":
-    cantidad = int(input("Elija si ingresará 2 ó 4 DNI: "))
-    while cantidad != 2 and cantidad != 4:
+    if opcion.lower() == "a":
         cantidad = int(input("Elija si ingresará 2 ó 4 DNI: "))
-    for i in range(1, cantidad+1):
-        entrada = input("Ingrese el DNI: ")
+        while cantidad != 2 and cantidad != 4:
+            cantidad = int(input("Elija si ingresará 2 ó 4 DNI: "))
+        for i in range(1, cantidad+1):
+            entrada = input("Ingrese el DNI: ")
         if entrada.isdigit():
             listaDNI.append(entrada)
     
-    for dni in listaDNI:
-        listaConjuntos.append(set(dni))
-        print(f"A partir del DNI {dni} se obtuvo el conjunto: {set(dni)}")
+        for dni in listaDNI:
+            listaConjuntos.append(set(dni))
+        
+        for i in range(0, len(listaConjuntos)):
+            print(f"A partir del DNI {listaDNI[i]} se obtuvo el conjunto: {listaConjuntos[i]}")
 
-    if len(listaDNI) == 2:
-        print("Operaciones con conjuntos")
-        a = set(listaDNI[0])
-        b = set(listaDNI[1])
-        print(f"El resultado de la unión entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.union(b)}")
-        print(f"El resutlado de la intersección entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.intersection(b)}")
-        print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.difference(b)}")
-        print(f"El resultado de la diferencia simétrica entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.symmetric_difference(b)}")
-    elif len(listaDNI) == 4:
-        a = set(listaDNI[0])
-        b = set(listaDNI[1])
-        c = set(listaDNI[2])
-        d = set(listaDNI[3])
-        print(f"El resultado de la unión entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.union(b)}")
-        print(f"El resultado de la unión entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.union(d)}")
-        print(f"El resutlado de la intersección entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.intersection(b)}")
-        print(f"El resutlado de la intersección entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.intersection(d)}")
-        print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.difference(b)}")
-        print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[1]} y {listaConjuntos[0]} es: {b.difference(a)}")
-        print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.difference(d)}")
-        print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[3]} y {listaConjuntos[2]} es: {d.difference(c)}")
-        print(f"El resultado de la diferencia simétrica entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.symmetric_difference(b)}")
-        print(f"El resultado de la diferencia simétrica entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.symmetric_difference(d)}")
+        if len(listaDNI) == 2:
+            print("Operaciones con conjuntos")
+            a = set(listaDNI[0])
+            b = set(listaDNI[1])
+            print(f"El resultado de la unión entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.union(b)}")
+            print(f"El resutlado de la intersección entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.intersection(b)}")
+            print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.difference(b)}")
+            print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[1]} y {listaConjuntos[0]} es: {b.difference(a)}")
+            print(f"El resultado de la diferencia simétrica entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.symmetric_difference(b)}")
+        elif len(listaDNI) == 4:
+            a = set(listaDNI[0])
+            b = set(listaDNI[1])
+            c = set(listaDNI[2])
+            d = set(listaDNI[3])
+            print(f"El resultado de la unión entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.union(b)}")
+            print(f"El resultado de la unión entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.union(d)}")
+            print(f"El resutlado de la intersección entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.intersection(b)}")
+            print(f"El resutlado de la intersección entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.intersection(d)}")
+            print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.difference(b)}")
+            print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[1]} y {listaConjuntos[0]} es: {b.difference(a)}")
+            print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.difference(d)}")
+            print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[3]} y {listaConjuntos[2]} es: {d.difference(c)}")
+            print(f"El resultado de la diferencia simétrica entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.symmetric_difference(b)}")
+            print(f"El resultado de la diferencia simétrica entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.symmetric_difference(d)}")
 
-    for dni in listaDNI:
-        string_dni = str(dni)
+        for dni in listaDNI:
+            string_dni = str(dni)
         for digito in set(dni):
             repite = string_dni.count(digito)
             print(f"El dígito {digito} se  repite {repite} veces en {string_dni}")
     
-    for dni in listaDNI:
-        num = int(dni)
+        for dni in listaDNI:
+            num = int(dni)
         print(f"La suma de los dígitos presentes en el DNI {dni} da como resultado: {suma_digitos(num)}")
     
-    for i in range(0, 10):
-        if digito_comun(listaConjuntos, str(i)) is True:
-            print(f"El dígito {i} forma parte de todos los conjuntos, entonces {i} es un número común")
+        for i in range(0, 10):
+            if digito_comun(listaConjuntos, str(i)) is True:
+                print(f"El dígito {i} forma parte de todos los conjuntos, entonces {i} es un número común")
     
-    contador = 0
-    for conjunto in listaConjuntos:
-        if len(conjunto) % 2 == 0:
-            contador += 1
+        contador = 0
+        for conjunto in listaConjuntos:
+            if len(conjunto) % 2 == 0:
+                contador += 1
 
-    if contador > (len(listaConjuntos) / 2):
-        print(f"De los {len(listaConjuntos)}, hay {contador} conjuntos con una cantidad par de elementos por ende el grupo se considera par")
+        if contador > (len(listaConjuntos) / 2):
+            print(f"De los {len(listaConjuntos)}, hay {contador} conjuntos con una cantidad par de elementos por ende el grupo se considera par")
 
-elif opcion.lower() == "b":
-    años = []
-    print("Por favor ingrese 4 años de nacimiento")
-    while len(años) < 4:
-        entrada = int(input(f"Ingrese el año de nacimiento: "))
-        if entrada in años:
-            print("Año repetido, ingrese un año distinto.")
+    elif opcion.lower() == "b":
+        años = []
+        print("Por favor ingrese 4 años de nacimiento")
+        while len(años) < 4:
+            entrada = int(input(f"Ingrese el año de nacimiento: "))
+            if entrada in años:
+                print("Año repetido, ingrese un año distinto.")
+            else:
+                años.append(entrada)
+
+        pares = 0
+        impares = 0
+        for entrada in años:
+            if entrada % 2 == 0:
+                pares += 1
+            else:
+                impares += 1
+        print(f"Cantidad de años pares: {pares}")
+        print(f"Cantidad de años impares: {impares}")
+        
+        if any(es_bisiesto(año) for año in años):
+                print("Tenemos un año especial.Es Bisiesto")
+        
+        if all(entrada > 2000 for entrada in años):
+            print("Todos los años ingresados pertenecen al grupo Z")
         else:
-            años.append(entrada)
+            print("Existe al menos un integrante del grupo que no pertenece al grupo Z")
 
-    pares = 0
-    impares = 0
-    for entrada in años:
-        if entrada % 2 == 0:
-            pares += 1
-        else:
-            impares += 1
-    print(f"Cantidad de años pares: {pares}")
-    print(f"Cantidad de años impares: {impares}")
     
-    if all(entrada > 2000 for entrada in años):
-        print("Todos los años ingresados pertenecen al grupo Z")
-else:
-    print("Opción incorrecta")
-
+print("\nFin del programa.")
 # Manejar el flujo del programa para que una vez que el usuario termino con la primera operación seleccionada
 # vuelva a l menu para ejecutar la otra opción si asi lo desea o terminar la ejecución.

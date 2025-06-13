8a5e8681935b6a84d194a70eff83559a7d269daf
NicoBalverdi <nicba96@gmail.com>
2025-06-13 01:30:41 -0300

    Se avanzo otro poco

--- Semana_Integracion_II.py
+++ Semana_Integracion_II.py
@@ -1,3 +1,17 @@
+# Funciones
+
+def digito_comun(lista_conjuntos, digito):
+    return all(digito in conjunto for conjunto in lista_conjuntos)
+
+def suma_digitos(numero):
+    suma = 0
+    while numero > 0:
+        suma += numero % 10
+        numero = numero // 10
+    return suma
+
+# Programa principal
+
 listaDNI = []
 listaConjuntos = []
 
@@ -39,21 +53,39 @@ if opcion.lower() == "a":
         print(f"El resutlado de la intersección entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.intersection(b)}")
         print(f"El resutlado de la intersección entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.intersection(d)}")
         print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.difference(b)}")
+        print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[1]} y {listaConjuntos[0]} es: {b.difference(a)}")
         print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.difference(d)}")
+        print(f"El resultado de la diferencia entre los conjuntos {listaConjuntos[3]} y {listaConjuntos[2]} es: {d.difference(c)}")
         print(f"El resultado de la diferencia simétrica entre los conjuntos {listaConjuntos[0]} y {listaConjuntos[1]} es: {a.symmetric_difference(b)}")
         print(f"El resultado de la diferencia simétrica entre los conjuntos {listaConjuntos[2]} y {listaConjuntos[3]} es: {c.symmetric_difference(d)}")
 
     for dni in listaDNI:
-        dni_str = str(dni)
+        string_dni = str(dni)
         for digito in set(dni):
-            repite = dni_str.count(digito)
-            print(f"El dígito {digito} se  repite {repite} veces en {dni_str}")
+            repite = string_dni.count(digito)
+            print(f"El dígito {digito} se  repite {repite} veces en {string_dni}")
     
-    # Evaluar condiciones lógicas (Si el hay un número común en los conjuntos)
-    # y la condición lógica con respecto a si los conjuntos ingresados tiene una cantidad par de elementos o no
+    for dni in listaDNI:
+        num = int(dni)
+        print(f"La suma de los dígitos presentes en el DNI {dni} da como resultado: {suma_digitos(num)}")
+    
+    for i in range(0, 10):
+        if digito_comun(listaConjuntos, str(i)) is True:
+            print(f"El dígito {i} forma parte de todos los conjuntos, entonces {i} es un número común")
+    
+    contador = 0
+    for conjunto in listaConjuntos:
+        if len(conjunto) % 2 == 0:
+            contador += 1
+
+    if contador > (len(listaConjuntos) / 2):
+        print(f"De los {len(listaConjuntos)}, hay {contador} conjuntos con una cantidad par de elementos por ende el grupo se considera par")
 
 elif opcion.lower() == "b":
     # Trabajo con los años de nacimiento (borrar el pass.)
     pass
 else:
     print("Opción incorrecta")
+
+# Manejar el flujo del programa para que una vez que el usuario termino con la primera operación seleccionada
+# vuelva a l menu para ejecutar la otra opción si asi lo desea o terminar la ejecución.
